734024190caf668f0612cb6afa1b81f5bd192c0b
Adrenalines <eevegorov@yandex.ru>
2019-10-19 23:36:52 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function createStore(rootReducer) {}
+
+const store$ = createStore();
+
+document.getElementById("increment").addEventListener("click", () => {});
+
+document.getElementById("decrement").addEventListener("click", () => {});
+
+document.getElementById("add").addEventListener("click", () => {});
